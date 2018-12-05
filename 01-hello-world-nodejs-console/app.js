7efb234b73ff30110cc34f4ec34804924a98a950
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-12-04 23:38:48 -0400

    adicao dos projetos

---
+++ 01-hello-world-nodejs-console/app.js
@@ -0,0 +1,11 @@
+/*
+  Oficina Como Criar Coisas Inteligentes com NodeJS e Arduino
+  I ETCEECAU - 05/12/2018
+  Facilitador:       Prof. Orlewilson Bentes Maia
+  Data Criacao:      27/11/2018
+  Data Atualizacao:  27/11/2018
+  Descricao:         Hello World NodeJS no console
+*/
+
+// mostra mensagem no console
+console.log("Hello World. Seja bem-vindo ao NodeJS!");
